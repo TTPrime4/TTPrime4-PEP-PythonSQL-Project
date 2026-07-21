6d397fccd7cca1e25fe98a8ae8339dccdd9948c6
TTPrime4 <TTPrime4@github.com>
2026-07-21 20:22:36 +0000

    Test run: 1/5 passed, 4 failed, 0 skipped - 2026-07-21T20:22:36.662Z

--- src/main/main.py
+++ src/main/main.py
@@ -86,7 +86,7 @@ def load_and_clean_call_logs(file_path):
 
     # Insert users into table, ending operation at abnormal data
     for i in list_logs:
-        if len(i) != 2:
+        if len(i) != 5:
             break
         cursor.execute("INSERT INTO logs (phoneNumber, startTime, endTime, direction, userId) VALUES (?, ?, ?, ?, ?)", i)
         conn.commit()
