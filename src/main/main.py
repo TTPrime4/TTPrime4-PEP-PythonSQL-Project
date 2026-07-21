5225e734151fe73360ecfcc88bd74a0606606247
TTPrime4 <TTPrime4@github.com>
2026-07-21 20:48:44 +0000

    Test run: 2/4 passed, 2 failed, 0 skipped - 2026-07-21T20:48:44.478Z

--- src/main/main.py
+++ src/main/main.py
@@ -64,16 +64,7 @@ def load_and_clean_users(file_path):
     # Insert users into table, ending operation at abnormal data
     for i in list_users:
         if len(i) != 2:
-            continue
-
-        # Test blank columns
-        noneFlag = 0
-        for column in i:
-            if column == '':
-                noneFlag = 1
-                break
-        if noneFlag == 1:
-            continue
+            break
         
         cursor.execute("INSERT INTO users (firstName, lastName) VALUES (?, ?)", i)
         conn.commit()
