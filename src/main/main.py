f188ffa885dfcf7a6d468269dc50bae71b2bfb8e
TTPrime4 <TTPrime4@github.com>
2026-07-21 20:08:33 +0000

    Test run: 0/5 passed, 5 failed, 0 skipped - 2026-07-21T20:08:32.995Z

--- src/main/main.py
+++ src/main/main.py
@@ -52,9 +52,10 @@ def load_and_clean_users(file_path):
     read_users = csv.reader(users)
     filter_users = []
     for i in read_users:
-        if len(i) != 2 :
+        if len(i) != 2:
             break
-        filter_users.append(i)
+        if i != ("firstName","lastName"):
+            filter_users.append(i)
     cursor.executemany("INSERT INTO users (firstName, lastName) VALUES (?, ?)", filter_users)
     
     conn.commit()
