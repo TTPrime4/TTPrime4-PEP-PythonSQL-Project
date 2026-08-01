20145ccc663fcf96a17c2b84d97b158ec31319a5
TTPrime4 <TTPrime4@github.com>
2026-08-01 23:27:34 +0000

    Test run: 3/4 passed, 1 failed, 0 skipped - 2026-08-01T23:27:34.966Z

--- src/main/main.py
+++ src/main/main.py
@@ -110,10 +110,12 @@ def load_and_clean_call_logs(file_path):
 # You must save records consisting of each userId, avgDuration, and numCalls
 # example: 1,105.0,4 - where 1 is the userId, 105.0 is the avgDuration, and 4 is the numCalls.
 def write_user_analytics(csv_file_path):
-    analytics = open(csv_file_path)
-    write_ana = csv.write(analytics)
-
-    cursor.execute("")
+    anal = cursor.execute("SELECT userId, AVG(endTime - startTime) as avgDuration, COUNT(userId) as numCalls FROM callLogs GROUP BY userId")
+    with open(csv_file_path, "a") as userAnal:
+        for i in anal:
+            userId, avgDuration, numCalls = i
+            row = str(userId) + "," + avgDuration + "," + numCalls + "\n"
+            userAnal.write(row)
     print("TODO: write_user_analytics")
 
 
